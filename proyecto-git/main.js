528924a220f6a70c3ec0f89d9f654e417d9d2bcc
MatiasValansi <valansimatias@gmail.com>
2025-03-27 20:39:49 -0300

    Agrega hora al saludo

--- proyecto-git/main.js
+++ proyecto-git/main.js
@@ -1,8 +1,4 @@
-// Función saludo usando template strings
-function saludo(nombre, mensaje) {
-    console.log(`${mensaje}, ${nombre}!`);
+// Rama "conflicto"
+function saludo(nombre, mensaje, hora) {
+    console.log(`${mensaje}, ${nombre}. Son las ${hora}.`);
     }
-    
-    // Ejemplos de uso:
-    saludo("Matías", "¡Hola"); // Resultado: ¡Hola, Matías!
-    saludo("Pepe", "¡Buen trabajo"); // Resultado: ¡Buen trabajo, Pepe!
